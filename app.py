1b3ac39adcc4dac722bfe8d5f0ae3b19470a443b
Fracnayn <fracnayn@gmail.com>
2025-09-22 13:36:38 +0400

    app.py

--- app.py
+++ app.py
@@ -2,6 +2,12 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+# Маршрут для главной страницы
+@app.route('/')
+def home():
+    return "Сервер мессенджера Fracnayn запущен!"
+
+# Маршрут для отправки сообщений
 @app.route('/send_message', methods=['POST'])
 def send_message():
     data = request.json
@@ -9,6 +15,3 @@ def send_message():
         print(f"Получено сообщение: {data.get('text')}")
         return {"status": "success", "message": "Сообщение получено"}
     return {"status": "error", "message": "Неверные данные"}, 400
-
-if __name__ == '__main__':
-    app.run(port=5000, debug=True)
